b6f5aa79fd824b590647e1a9b1d37a908c678f27
urbanjagodic <34213584+urbanjagodic@users.noreply.github.com>
2018-11-18 00:48:22 +0100

    # added validation methods, Utils class, packages

---
+++ app/src/main/java/serve/serveup/utils/Utils.java
@@ -0,0 +1,47 @@
+package serve.serveup.utils;
+
+import java.util.regex.Pattern;
+
+
+/**
+ * author: urban.jagodic
+ * Class for validating input.
+ * Tool class with static methods for
+ * easier validation checks.
+ */
+public class Utils {
+
+    public static boolean isNullOrEmpty(String input) {
+        return input == null || input.trim().length() == 0;
+    }
+
+
+    // check validity of email address
+    public static boolean isEmailValid(String email){
+        if(!isNullOrEmpty(email)) {
+            String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\." +
+                    "[a-zA-Z0-9_+&*-]+)*@" +
+                    "(?:[a-zA-Z0-9-]+\\.)+[a-z" +
+                    "A-Z]{2,7}$";
+            Pattern pat = Pattern.compile(emailRegex);
+            return pat.matcher(email).matches();
+        }
+        return false;
+    }
+
+    //check passwordValidity
+    // must contain at least 6 chars, one upper-case letter and a number
+    // (?=.*\W) for non-word char
+    public static boolean isPasswordValid(String password) {
+        if(!isNullOrEmpty(password)) {
+            String passwordRegex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).{6,}$";
+            return isInRegex(password, passwordRegex);
+        }
+        return false;
+    }
+
+    public static boolean isInRegex(String input, String regex) {
+        Pattern pat = Pattern.compile(regex);
+        return pat.matcher(input).matches();
+    }
+}
--- app/src/main/java/serve/serveup/views/MainActivity.java
+++ app/src/main/java/serve/serveup/views/MainActivity.java
@@ -1,7 +1,9 @@
-package serve.serveup;
+package serve.serveup.views;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import serve.serveup.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_login);
+
     }
 }
