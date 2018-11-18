a96e2c9b3e1ae44b21038214e77c664be7282296
urbanjagodic <34213584+urbanjagodic@users.noreply.github.com>
2018-11-18 15:42:49 +0100

    # added validation function on click for email, password, Google sign in class, but the sign in doesnt work yet.

--- app/src/main/java/serve/serveup/views/MainActivity.java
+++ app/src/main/java/serve/serveup/views/MainActivity.java
@@ -1,16 +1,28 @@
 package serve.serveup.views;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import serve.serveup.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LoginFragment.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_login);
 
+        LoginFragment myLoginFrag = new LoginFragment();
+
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.fragment_container, myLoginFrag).commit();
+
+
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+        // fragment interaction
     }
 }
