6180fb47d05344cac6ea1f7cb611173dfa938a92
urbanjagodic <34213584+urbanjagodic@users.noreply.github.com>
2019-01-12 21:06:25 +0100

    # couple fixes, basket visual refresh, set Icon to the whole app,   design changes ...

--- app/src/main/java/serve/serveup/views/navigation/BasketFragment.java
+++ app/src/main/java/serve/serveup/views/navigation/BasketFragment.java
@@ -15,6 +15,7 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import serve.serveup.R;
+import serve.serveup.utils.ContentStore;
 import serve.serveup.utils.Utils;
 import serve.serveup.utils.adapters.ShoppingBasketItemAdapter;
 import serve.serveup.views.order.PaymentOptionActivity;
@@ -33,6 +34,7 @@ public class BasketFragment extends Fragment {
     private TextView totalMinutesText;
     private int totalMinutes = 10;
     private int minutes = 10;
+    private ContentStore cntStore;
 
     public BasketFragment() {
         // Required empty public constructor
@@ -55,28 +57,31 @@ public class BasketFragment extends Fragment {
         // Initialize the view components
         shoppingBasketRecycleView = rootView.findViewById(R.id.shoppingBasketRecyclerView);
         linearLayoutManager = new LinearLayoutManager(getActivity());
-        shoppingBasketItemAdapter = new ShoppingBasketItemAdapter(getActivity());
+        shoppingBasketRecycleView.setLayoutManager(linearLayoutManager);
 
         // Initialize view components for expected arrival time
         incMinutesButton = rootView.findViewById(R.id.incMinutes);
         decMinutesButton = rootView.findViewById(R.id.decMinutes);
         totalMinutesText = rootView.findViewById(R.id.minute);
+        cntStore = new ContentStore(getActivity());
 
         setIncrAndDecrMinutes();
-
-        // Set the layout manager and the adapter of the Recycler View
-        shoppingBasketRecycleView.setLayoutManager(linearLayoutManager);
-        shoppingBasketRecycleView.setAdapter(shoppingBasketItemAdapter);
+        updateCurrentBasket();
 
         emptyBasketText.setVisibility(shoppingBasketItemAdapter.getItemCount() < 1 ? View.VISIBLE : View.GONE);
         
         makeOrderButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String casPrevzema = Utils.createDateTimeString(String.valueOf(totalMinutes));
-                Intent myIntent = new Intent(getActivity(), PaymentOptionActivity.class);
-                myIntent.putExtra("order_pickup_time", casPrevzema);
-                startActivity(myIntent);
+
+                if(cntStore.getSession().mealsNotEmpty()) {
+                    String casPrevzema = Utils.createDateTimeString(String.valueOf(totalMinutes));
+                    Intent myIntent = new Intent(getActivity(), PaymentOptionActivity.class);
+                    myIntent.putExtra("order_pickup_time", casPrevzema);
+                    startActivity(myIntent);
+                }
+                else
+                    Utils.showToast(getActivity(), "Vaša košarica je prazna");
             }
         });
 
@@ -128,4 +133,24 @@ public class BasketFragment extends Fragment {
     public interface OnFragmentInteractionListener {
         void onFragmentInteraction(Uri uri);
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Utils.logInfo("Onstart");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        updateCurrentBasket();
+    }
+
+
+    public void updateCurrentBasket() {
+        shoppingBasketItemAdapter = new ShoppingBasketItemAdapter(getActivity());
+        shoppingBasketRecycleView.setAdapter(shoppingBasketItemAdapter);
+        emptyBasketText.setVisibility(shoppingBasketItemAdapter.getItemCount() < 1 ? View.VISIBLE : View.GONE);
+    }
+
 }
